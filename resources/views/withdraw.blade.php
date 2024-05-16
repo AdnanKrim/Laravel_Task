33fe60ee7fabb4b0e34ef4a308338abfb832a36d
AdnanKrim <adnankarimctg@gmail.com>
2024-05-16 20:03:03 +0600

    final committt

--- resources/views/withdraw.blade.php
+++ resources/views/withdraw.blade.php
@@ -33,7 +33,7 @@
         <input type="date" name="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Date">
       </div>
     
-    <button type="submit" class="btn btn-primary">Deposite</button>
+    <button type="submit" class="btn btn-primary">Withdraw</button>
   </form>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     
